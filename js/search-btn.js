8daee2d06251bc27d35dd999cebf7a250cfb9592
Dmitry Ulanov <ulanov2015ds@gmail.com>
2022-09-20 06:10:08 +0700

    added dropdown list structure

--- js/search-btn.js
+++ js/search-btn.js
@@ -3,7 +3,6 @@ let btnSearchClose = document.querySelector('.search-top__icon-close')
 let inputForm = document.querySelector('.header__search-top')
 
 btnSearchOpen.addEventListener('click', function() {
-  // btnSearchOpen.remove('search-top__btn')
   inputForm.classList.add('header__search-top--active')
 })
 
