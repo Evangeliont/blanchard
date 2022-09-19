be1547d25179d0595c75b85510ec6cd36743382a
Dmitry Ulanov <ulanov2015ds@gmail.com>
2022-09-19 08:44:49 +0700

    wrote a adaptive form search

--- js/search-btn.js
+++ js/search-btn.js
@@ -1,11 +1,12 @@
 let btnSearchOpen = document.querySelector('.search-top__btn')
 let btnSearchClose = document.querySelector('.search-top__icon-close')
-let inputForm = document.querySelector('.search-top__wrapper')
+let inputForm = document.querySelector('.header__search-top')
 
 btnSearchOpen.addEventListener('click', function() {
-  inputForm.classList.add('search-top__wrapper--active')
+  // btnSearchOpen.remove('search-top__btn')
+  inputForm.classList.add('header__search-top--active')
 })
 
 btnSearchClose.addEventListener('click', function() {
-  inputForm.classList.remove('search-top__wrapper--active')
+  inputForm.classList.remove('header__search-top--active')
 })
