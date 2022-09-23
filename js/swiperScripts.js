d74d5cfdba9dbd2d18446ea9cd290abaf30a1b93
Dmitry Ulanov <ulanov2015ds@gmail.com>
2022-09-23 07:07:12 +0700

    section hero and added plugin swiper for effect fade

---
+++ js/swiperScripts.js
@@ -0,0 +1,10 @@
+// const swiperHero = document.querySelector('.hero')
+const swiper = new Swiper('.hero__swiper', {
+  slidesPerView: 1,
+  speed: 2000,
+  autoplay: {
+    delay: 2000
+  },
+  effect: 'fade',
+  allowTouchMove: false,
+})
