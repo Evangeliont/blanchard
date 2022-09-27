7f50e53e863dacf218438984f075d1d44f5d7468
Dmitry Ulanov <ulanov2015ds@gmail.com>
2022-09-28 02:38:42 +0700

    added slider in gallery

--- js/choices-plugin.js
+++ js/choices-plugin.js
@@ -2,4 +2,4 @@ const element = document.querySelector('#selectCustom');
 const choices = new Choices(element, {
   allowHTML: true,
   searchEnabled: false,
-});
+})
--- js/swiperScripts.js
+++ js/swiperScripts.js
@@ -9,3 +9,26 @@ const swiper = new Swiper('.hero__swiper', {
   allowTouchMove: false,
 })
 
+
+const gallerySlider = new Swiper(".gallery__slider", {
+  slidesPerView: 3,
+  slidesPerGroup: 3,
+    keyboard: {
+    enabled: true,
+    onlyInViewport: true,
+     pageUpDown: true,
+  },
+
+  spaceBetween: 50,
+  navigation: {
+    nextEl: ".gallery__slider-btn--next",
+    prevEl: ".gallery__slider-btn--prev",
+  },
+
+  a11y: {
+    prevSlideMessage: 'Предыдущий',
+    nextSlideMessage: 'Следующий',
+  }
+});
+
+
