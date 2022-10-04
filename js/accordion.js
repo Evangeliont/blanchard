ef795a814a84488cd5169e58a83b853f186ff603
Dmitry Ulanov <ulanov2015ds@gmail.com>
2022-10-05 02:37:50 +0700

    added catalog section and accordion plugin. Write logic for accordion

---
+++ js/accordion.js
@@ -0,0 +1,9 @@
+let accordion = new Accordion('.accordion__list', {
+  duration: 300,
+  elementClass:	'accordion__item',
+  triggerClass: 'accordion__top',
+  panelClass: 'accordion__bottom',
+  showMultiple: false,
+  openOnInit: [0],
+  collapse: true
+})
