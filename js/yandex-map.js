fa57ef0f327817a642dfbb8ba7c54f84f9691a06
Dmitry Ulanov <ulanov2015ds@gmail.com>
2022-10-10 01:49:28 +0700

    added style for validation fields and added marker for map section

--- js/yandex-map.js
+++ js/yandex-map.js
@@ -2,18 +2,24 @@
     // загрузятся все компоненты API, а также когда будет готово DOM-дерево.
     ymaps.ready(init);
     function init(){
-        // Создание карты.
-        var myMap = new ymaps.Map("map", {
-            // Координаты центра карты.
-            // Порядок по умолчанию: «широта, долгота».
-            // Чтобы не определять координаты центра карты вручную,
-            // воспользуйтесь инструментом Определение координат.
-            center: [55.76, 37.64],
-            // Уровень масштабирования. Допустимые значения:
-            // от 0 (весь мир) до 19.
-            zoom: 14
-        });
-
+        let center = [55.758468, 37.601088]
+        let myMap = new ymaps.Map("map", {
+            center: center,
+            zoom: 14,
+            controls: ["smallMapDefaultSet"],
+        })
+        let placeMarkCustom = new ymaps.Placemark(
+          center,
+          {
+            balloonContent: "Леонтьевский переулок, дом 5/1",
+          },
+          {
+            iconLayout: "default#image",
+            iconImageHref: "../img/yandex-map/marker.svg",
+            iconImageSize: [20, 20],
+            iconImageOffset: [0, 0],
+          }
+        )
         myMap.controls.remove("searchControl");
         myMap.controls.remove("trafficControl");
         myMap.controls.remove("fullscreenControl");
@@ -22,5 +28,5 @@
         myMap.behaviors.disable("multiTouch");
         myMap.behaviors.disable("drag");
         myMap.controls.remove("typeSelector");
-        // myMap.geoObjects.add(placeMarkCustom);
+        myMap.geoObjects.add(placeMarkCustom);
     }
