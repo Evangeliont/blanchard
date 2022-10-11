3b85876da5bda20733d430c44a1862d87e73ffd9
Dmitry Ulanov <ulanov2015ds@gmail.com>
2022-10-12 03:52:21 +0700

    write style for tooltips

--- js/smooth-scrolling.js
+++ js/smooth-scrolling.js
@@ -1,24 +1,24 @@
-const menuLinks = document.querySelectorAll(".header__link[data-goto]");
+const menuLinks = document.querySelectorAll('.header__link[data-goto]')
 
 if (menuLinks.length > 0) {
   menuLinks.forEach((menuLink) => {
-    menuLink.addEventListener("click", onMenuLinkClick);
-  });
+    menuLink.addEventListener('click', onMenuLinkClick)
+  })
 
   function onMenuLinkClick(e) {
-    const menuLink = e.target;
+    const menuLink = e.target
 
     //проверка есть ли такой атрибут
     if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
-      const gotoBlock = document.querySelector(menuLink.dataset.goto);
+      const gotoBlock = document.querySelector(menuLink.dataset.goto)
       const gotoBlockValue =
-        gotoBlock.getBoundingClientRect().top + pageYOffset - document.querySelector("header").offsetHeight;
+        gotoBlock.getBoundingClientRect().top + pageYOffset - document.querySelector('header').offsetHeight
 
       window.scrollTo({
         top: gotoBlockValue,
-        behavior: "smooth",
-      });
-      e.preventDefault();
+        behavior: 'smooth',
+      })
+      e.preventDefault()
     }
   }
 }
